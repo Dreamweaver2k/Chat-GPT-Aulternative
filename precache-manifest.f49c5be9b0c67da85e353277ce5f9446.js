cd58aac4e267fb53c78f26925e5dae99a5591710
DreamWeaver2k <chandlerault@gmail.com>
2023-02-04 18:40:50 -0800

    Updates

---
+++ precache-manifest.f49c5be9b0c67da85e353277ce5f9446.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcedcb28eb50419983ac",
+    "url": "/Chat-GPT-Aulternative/static/css/main.189f00de.chunk.css"
+  },
+  {
+    "revision": "bcedcb28eb50419983ac",
+    "url": "/Chat-GPT-Aulternative/static/js/main.50b7bbe9.chunk.js"
+  },
+  {
+    "revision": "6533bbd04c1275912876",
+    "url": "/Chat-GPT-Aulternative/static/js/runtime~main.1bfd44fa.js"
+  },
+  {
+    "revision": "27a549c966fffa76a81d",
+    "url": "/Chat-GPT-Aulternative/static/css/2.b2b43d99.chunk.css"
+  },
+  {
+    "revision": "27a549c966fffa76a81d",
+    "url": "/Chat-GPT-Aulternative/static/js/2.fed92f3a.chunk.js"
+  },
+  {
+    "revision": "43cd0406631cf17174cd",
+    "url": "/Chat-GPT-Aulternative/static/js/3.294c8d66.chunk.js"
+  },
+  {
+    "revision": "4d3d89fb138e3572eaf32edd8293a109",
+    "url": "/Chat-GPT-Aulternative/static/media/GreyBox.4d3d89fb.svg"
+  },
+  {
+    "revision": "d0cf9130e691d9a52b010de6d4d9030d",
+    "url": "/Chat-GPT-Aulternative/index.html"
+  }
+];
