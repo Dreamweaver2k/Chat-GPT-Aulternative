aba8807af698dc0a2e21b332d3e254de873ebd19
DreamWeaver2k <chandlerault@gmail.com>
2023-02-04 18:32:55 -0800

    another commit

---
+++ build/precache-manifest.e4763f111118f0527c5cb06b7d7fc04f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcedcb28eb50419983ac",
+    "url": "/static/css/main.189f00de.chunk.css"
+  },
+  {
+    "revision": "bcedcb28eb50419983ac",
+    "url": "/static/js/main.50b7bbe9.chunk.js"
+  },
+  {
+    "revision": "657d5122646de4bf8856",
+    "url": "/static/js/runtime~main.2e94cb40.js"
+  },
+  {
+    "revision": "bbe32b71dba96e696670",
+    "url": "/static/css/2.b2b43d99.chunk.css"
+  },
+  {
+    "revision": "bbe32b71dba96e696670",
+    "url": "/static/js/2.04dfe099.chunk.js"
+  },
+  {
+    "revision": "43cd0406631cf17174cd",
+    "url": "/static/js/3.294c8d66.chunk.js"
+  },
+  {
+    "revision": "4d3d89fb138e3572eaf32edd8293a109",
+    "url": "/static/media/GreyBox.4d3d89fb.svg"
+  },
+  {
+    "revision": "1e96ff175b7f20b303dcfd7840be7df0",
+    "url": "/index.html"
+  }
+];
